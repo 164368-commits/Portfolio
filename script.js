5167fe7c03c6509b96052b778b57555414781a85
Rp1331 <164368@wsk.ac.th>
2026-07-21 11:45:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// แสดงข้อความต้อนรับ
+window.onload = function () {
+    alert("ยินดีต้อนรับสู่ Portfolio ของ Ronnaphop");
+};
+
+// ปุ่ม Dark Mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
