2041588d29718c0267710af19afd5270a5802308
Rp1331 <164368@wsk.ac.th>
2026-07-21 14:52:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
-let img = [
+let img=[
 
 "SWU_10090110702501A-0-D0101_1749800445651_page-0001.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-0002.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-0003.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-0004.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-0005.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-0006.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-0007.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-0008.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-0009.jpg",
-
 "SWU_10090110702501A-0-D0101_1749800445651_page-00010.jpg"
 
 ];
 
 
-let index = 0;
+let index=0;
 
 
-// เปิดรูปใหญ่
 
 function openImage(i){
 
-index = i;
+index=i;
 
-document.getElementById("big").src = img[index];
+document.getElementById("big").src=img[index];
 
-document.getElementById("lightbox").style.display = "flex";
+document.getElementById("lightbox").style.display="flex";
 
 }
 
 
-
-// เลื่อนไปข้างหน้า
 
 function next(){
 
@@ -48,17 +36,15 @@ index++;
 
 if(index >= img.length){
 
-index = 0;
+index=0;
 
 }
 
-document.getElementById("big").src = img[index];
+document.getElementById("big").src=img[index];
 
 }
 
 
-
-// เลื่อนย้อนกลับ
 
 function prev(){
 
@@ -66,24 +52,24 @@ index--;
 
 if(index < 0){
 
-index = img.length - 1;
+index=img.length-1;
 
 }
 
-document.getElementById("big").src = img[index];
+document.getElementById("big").src=img[index];
 
 }
 
 
 
-// ปิดรูป
+// คลิกพื้นหลังเพื่อปิด
 
-document.getElementById("lightbox").onclick = function(e){
+document.getElementById("lightbox").onclick=function(e){
 
-if(e.target.id == "lightbox"){
+if(e.target.id=="lightbox"){
 
-this.style.display = "none";
+document.getElementById("lightbox").style.display="none";
 
 }
 
-};
+}
